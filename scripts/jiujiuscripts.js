8e6d69d975e55d586de0067d18f97a14ec1a6f6f
cgdgy <chen_gao_ding@163.com>
2017-02-02 01:03:05 +0800

    programming the nabber

---
+++ scripts/jiujiuscripts.js
@@ -0,0 +1,14 @@
+(function(doc,win){
+    var docEI=doc.documentElement;
+    resizeEvt='orientationchange'in window?'orientationchange':'resize';
+    recalc=function(){
+        var clientWidth=docEI.clientWidth;
+        if(!clientWidth) return;
+        if(clientWidth<420)
+        docEI.style.fontSize=20*(clientWidth/375)+'px';
+        };
+    if(!doc.addEventListener) return;
+    win.addEventListener(resizeEvt,recalc,false);
+    doc.addEventListener('DOMContentLoaded',recalc,false);
+}
+)(document,window);
