a26db31f4b715cb0107187288171a7c3e951704e
cgdgy <chen_gao_ding@163.com>
2017-02-07 00:24:26 +0800

    navigation finished

--- scripts/jiujiuscripts.js
+++ scripts/jiujiuscripts.js
@@ -13,26 +13,43 @@
 }
 )(document,window);
 /*toggle点开后后续元素下滑效果*/
-var slideSwitcher=false;/*下滑切换标记*/
+var toggleSwitcher=false;/*toggle点击后下滑切换标记*/
+var dropdownSwitcher=false;/*dropdown点击后下滑切换标记*/
 $(function(){
     $(".navbar-toggle").click(function(){
-        if(slideSwitcher==false){
-            collapseUpDown($(".jiujiuItem"),true,$(".collapse"));
-            slideSwitcher=true;
+        var refHeight=($(".navbar-toggle").height())/2;/*获取页面基础网格高度1rem*/
+        if(toggleSwitcher==false){
+            slideHeight=5*refHeight;
+            collapseUpDown($(".jiujiuItem"),true,slideHeight);
+            toggleSwitcher=true;
         }
         else{
-            collapseUpDown($(".jiujiuItem"),false,$(".collapse"));
-            slideSwitcher=false;
+            slideHeight=($(".collapse").height())+2*refHeight;
+            collapseUpDown($(".jiujiuItem"),false,slideHeight);
+            toggleSwitcher=false;
+            dropdownSwitcher=false;  
+        }
+    });
+    $(".dropdown-toggle").click(function(){
+        var refHeight=($(".navbar-toggle").height())/2;
+        var sndmenuNum=$(".dropdown-menu>li").length;
+        var sndslideHeight=3*refHeight*sndmenuNum+10+2*refHeight;
+        if(dropdownSwitcher==false){
+            collapseUpDown($(".jiujiuItem"),true,sndslideHeight);
+            dropdownSwitcher=true;
+        }
+        else{
+            collapseUpDown($(".jiujiuItem"),false,sndslideHeight);
+            dropdownSwitcher=false;            
         }
     })
 });
-function collapseUpDown(Item,updown,heightEle){
-    var slideHeight=$(heightEle).height();
+function collapseUpDown(item,updown,slideDistance){
     if(updown==true){
-        $(Item).stop().animate({marginTop:"+="+slideHeight},200);
+        $(item).stop().animate({marginTop:"+="+slideDistance},200);
     }
     else{
-        $(Item).stop().animate({marginTop:"+="+(-slideHeight)},200);
+        $(item).stop().animate({marginTop:"-="+slideDistance},200);
     }
 }
 
